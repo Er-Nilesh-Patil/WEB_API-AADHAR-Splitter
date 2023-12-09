32d18fa9be79a5cd9c85878ad5ba651065ebfbe0
Er-Nilesh-Patil <ernileshpatil2016@gmail.com>
2023-12-09 17:37:38 +0530

    improved the alogoritham method by using linq in it

--- AadhaarSplitterAPI/Services/AadhaarService.cs
+++ AadhaarSplitterAPI/Services/AadhaarService.cs
@@ -23,44 +23,8 @@ namespace AadhaarSplitterAPI.Services
         /// Extracts Aadhaar numbers from the given raw text.
         /// </summary>
         /// <param name="rawText">The raw text to extract Aadhaar numbers from.</param>
-        /// <returns>A list of valid Aadhaar numbers.</returns>
-      
-        
-        /*        public List<string> ExtractAadhaarNumbers(string rawText)
-                {
-                    try
-                    {
-                        string pattern = @"\b\d{12}\b";
-                        Regex regex = new Regex(pattern);
-
-                        MatchCollection matches = regex.Matches(rawText);
-
-                        List<string> validAadhaarNumbers = new List<string>();
-                        foreach (Match match in matches)
-                        {
-                            string aadhaarNumber = ValidateAndManipulate(match.Value);
-
-                            if (IsValidAadhaarNumber(aadhaarNumber))
-                            {
-                                validAadhaarNumbers.Add(aadhaarNumber);
-                            }
-                        }
-
-                        _logger.LogInformation("Extracted Aadhaar numbers: {AadhaarNumbers}", string.Join(", ", validAadhaarNumbers));
-
-                        return validAadhaarNumbers;
-                    }
-                    catch (Exception ex)
-                    {
-                        _logger.LogError(ex, "Error extracting Aadhaar numbers");
-                        throw; // Re-throw the exception for higher-level handling
-                    }
-                }
-        */
-        
-        ///<summary
-         //Note that this approach assumes that Aadhaar numbers can span multiple lines and blocks
-        ///>
+        /// <returns>A list of valid Aadhaar numbers.</returns>      
+       
         public List<string> ExtractAadhaarNumbers(string rawText)
         {
             try
@@ -100,6 +64,38 @@ namespace AadhaarSplitterAPI.Services
                 throw; // Re-throw the exception for higher-level handling
             }
         }
+
+        /*        public List<string> ExtractAadhaarNumbers(string rawText)
+        {
+            try
+            {
+                string pattern = @"\b\d{12}\b";
+                Regex regex = new Regex(pattern);
+
+                MatchCollection matches = regex.Matches(rawText);
+
+                List<string> validAadhaarNumbers = new List<string>();
+                foreach (Match match in matches)
+                {
+                    string aadhaarNumber = ValidateAndManipulate(match.Value);
+
+                    if (IsValidAadhaarNumber(aadhaarNumber))
+                    {
+                        validAadhaarNumbers.Add(aadhaarNumber);
+                    }
+                }
+
+                _logger.LogInformation("Extracted Aadhaar numbers: {AadhaarNumbers}", string.Join(", ", validAadhaarNumbers));
+
+                return validAadhaarNumbers;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error extracting Aadhaar numbers");
+                throw; // Re-throw the exception for higher-level handling
+            }
+        }
+*/
 
         private string ValidateAndManipulate(string aadhaarNumber)
         {
@@ -165,50 +161,44 @@ namespace AadhaarSplitterAPI.Services
         {
             try
             {
-                int[][] d = {
-                new int[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9},
-                new int[] {1, 2, 3, 4, 0, 6, 7, 8, 9, 5},
-                new int[] {2, 3, 4, 0, 1, 7, 8, 9, 5, 6},
-                new int[] {3, 4, 0, 1, 2, 8, 9, 5, 6, 7},
-                new int[] {4, 0, 1, 2, 3, 9, 5, 6, 7, 8},
-                new int[] {5, 9, 8, 7, 6, 0, 4, 3, 2, 1},
-                new int[] {6, 5, 9, 8, 7, 1, 0, 4, 3, 2},
-                new int[] {7, 6, 5, 9, 8, 2, 1, 0, 4, 3},
-                new int[] {8, 7, 6, 5, 9, 3, 2, 1, 0, 4},
-                new int[] {9, 8, 7, 6, 5, 4, 3, 2, 1, 0}
-            };
-
-                int[][] p = {
-                new int[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9},
-                new int[] {1, 5, 7, 6, 2, 8, 3, 0, 9, 4},
-                new int[] {5, 8, 0, 3, 7, 9, 6, 1, 4, 2},
-                new int[] {8, 9, 1, 6, 0, 4, 3, 5, 2, 7},
-                new int[] {9, 4, 5, 3, 1, 2, 6, 8, 7, 0},
-                new int[] {4, 2, 8, 6, 5, 7, 3, 9, 0, 1},
-                new int[] {2, 7, 9, 3, 8, 0, 6, 4, 1, 5},
-                new int[] {7, 0, 4, 6, 9, 1, 3, 2, 5, 8}
-            };
-
-                int[] inv = new int[] { 0, 4, 3, 2, 1, 5, 6, 7, 8, 9 };
-
-                int c = 0;
-                int[] myArray = new int[aadhaarNumber.Length];
-                for (int i = 0; i < aadhaarNumber.Length; i++)
+                int[][] d =
                 {
-                    myArray[i] = int.Parse(aadhaarNumber[i].ToString());
-                }
+            new [] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9},
+            new [] {1, 2, 3, 4, 0, 6, 7, 8, 9, 5},
+            new [] {2, 3, 4, 0, 1, 7, 8, 9, 5, 6},
+            new [] {3, 4, 0, 1, 2, 8, 9, 5, 6, 7},
+            new [] {4, 0, 1, 2, 3, 9, 5, 6, 7, 8},
+            new [] {5, 9, 8, 7, 6, 0, 4, 3, 2, 1},
+            new [] {6, 5, 9, 8, 7, 1, 0, 4, 3, 2},
+            new [] {7, 6, 5, 9, 8, 2, 1, 0, 4, 3},
+            new [] {8, 7, 6, 5, 9, 3, 2, 1, 0, 4},
+            new [] {9, 8, 7, 6, 5, 4, 3, 2, 1, 0}
+        };
 
-                for (int i = 0; i < myArray.Length; i++)
+                int[][] p =
                 {
-                    c = d[c][p[i % 8][myArray[i]]];
-                }
+            new [] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9},
+            new [] {1, 5, 7, 6, 2, 8, 3, 0, 9, 4},
+            new [] {5, 8, 0, 3, 7, 9, 6, 1, 4, 2},
+            new [] {8, 9, 1, 6, 0, 4, 3, 5, 2, 7},
+            new [] {9, 4, 5, 3, 1, 2, 6, 8, 7, 0},
+            new [] {4, 2, 8, 6, 5, 7, 3, 9, 0, 1},
+            new [] {2, 7, 9, 3, 8, 0, 6, 4, 1, 5},
+            new [] {7, 0, 4, 6, 9, 1, 3, 2, 5, 8}
+        };
+
+                int[] inv = { 0, 4, 3, 2, 1, 5, 6, 7, 8, 9 };
+
+                int c = aadhaarNumber
+                    .Select(ch => int.Parse(ch.ToString()))
+                    .Aggregate(0, (current, t) => d[current][p[current % 8][t]]);
 
                 return c == 0;
             }
             catch (Exception ex)
             {
                 _logger.LogError(ex, "Error verifying Aadhaar number with Verhoeff algorithm");
-                throw; // Re-throw the exception for higher-level handling
+                throw; 
             }
         }
     }
